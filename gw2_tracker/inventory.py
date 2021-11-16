612baf2d0cd556660284e492183f080e18241ed1
Quentin Soubeyran <45202794+QuentinSoubeyran@users.noreply.github.com>
2021-11-16 21:46:53 +0100

    corrected inventory

--- gw2_tracker/inventory.py
+++ gw2_tracker/inventory.py
@@ -10,14 +10,13 @@ Main features :
 """
 from __future__ import annotations
 
-import functools
 import json
 import threading
 import types
 from collections import abc
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import IO, Mapping
+from typing import IO, Any, Mapping
 
 from gw2_tracker import gw2_api
 
@@ -34,27 +33,53 @@ class Inventory(abc.Mapping[str, int]):
     as sets and the values one-by-one. This is performed by checking the
     difference of the compared set to 0.
     """
+
     content: Mapping[str, int] = field(default_factory=dict)
 
     @classmethod
     def from_file(cls, file: str | Path | IO[bytes | str]) -> Inventory:
+        """
+        Deserialize an `Inventory` from a file
+
+        Arguments:
+            file: file name, `Path` or opened file to read from
+
+        Returns:
+            deserialized `Inventory` object
+
+        Raises:
+            ValueError: the file is not in the correct format
+        """
         if hasattr(file, "read"):
             content = json.loads(file.read())
         else:
             with Path(file).open("rt", encoding="utf-8") as f:
                 content = json.load(f)
+        return cls.from_json(content)
+
+    @classmethod
+    def from_json(cls, obj: Any) -> Inventory:
+        """
+        Deserialize an `Inventory` from an already loaded JSON
+
+        Argument:
+            obj: json object to deserialize
+
+        Returns:
+            deserialized `Inventory` object
+
+        Raises:
+            ValueError: the json object is not in the correct format
+        """
         if not (
-            isinstance(content, dict)
-            and all(
-                isinstance(k, str) and isinstance(v, int) for k, v in content.items()
-            )
+            isinstance(obj, dict)
+            and all(isinstance(k, str) and isinstance(v, int) for k, v in obj.items())
         ):
-            raise ValueError(
-                f"json file should containt an dict[str, int], got {content}"
-            )
-        return Inventory(content)
+            raise ValueError(f"json should be a dict[str, int], got {obj}")
+        return Inventory(obj)
 
     def __post_init__(self):
+        # make content immutable
         self.__dict__["content"] = types.MappingProxyType(
             {k: v for k, v in self.content.items() if v}
         )
@@ -70,16 +95,22 @@ class Inventory(abc.Mapping[str, int]):
 
     def __add__(self, other: Inventory) -> Inventory:
         if isinstance(other, Inventory):
-            return Inventory({
-                k: self.get(k, 0) + other.get(k, 0) for k in self.keys() | other.keys()
-            })
+            return Inventory(
+                {
+                    k: self.get(k, 0) + other.get(k, 0)
+                    for k in self.keys() | other.keys()
+                }
+            )
         return NotImplemented
 
     def __sub__(self, other: Inventory) -> Inventory:
         if isinstance(other, Inventory):
-            return Inventory({
-                k: self.get(k, 0) - other.get(k, 0) for k in self.keys() | other.keys()
-            })
+            return Inventory(
+                {
+                    k: self.get(k, 0) - other.get(k, 0)
+                    for k in self.keys() | other.keys()
+                }
+            )
         return NotImplemented
 
     def __lt__(self, other: int | Inventory) -> bool:
@@ -112,9 +143,16 @@ class Inventory(abc.Mapping[str, int]):
             return diff and diff > 0
 
     def to_json(self) -> dict[str, int]:
+        """Serialize the instance to json format"""
         return dict(self.content)
 
     def to_file(self, file: str | Path | IO[str], indent=4, sort_keys=True, **kwargs):
+        """Serialize the instance to json format and write to the file
+
+        Arguments:
+            file: file name, `Path` or `IO` object to write to
+            kwargs: additional keywords argument to pass to `json.dump`
+        """
         kwargs = kwargs | dict(indent=indent, sort_keys=sort_keys)
         content = self.to_json()
         if hasattr(file, "write"):
