ff001c299fe192e62d5096e62beab813d76c3116
Quentin Soubeyran <45202794+QuentinSoubeyran@users.noreply.github.com>
2021-11-19 18:39:01 +0100

    moved network code to gw2_api.py; moving to aiohttp library

--- gw2_tracker/gw2_api.py
+++ gw2_tracker/gw2_api.py
@@ -14,91 +14,258 @@ Main features :
 
 @author: Krashnark
 """
+import asyncio
+import functools
 import os
+import threading
+from asyncio.events import AbstractEventLoop
+from typing import Any, Final, NewType, Type, TypeAlias, TypedDict, TypeVar
 
+import aiohttp
 import requests
+from attr import define
+
+from gw2_tracker.inventory import Inventory, Snapshot
+from gw2_tracker.utils import autoformat
+
+# Types aliases
+T = TypeVar("T")
+APIKey: TypeAlias = NewType("APIKey", str)
+URL: TypeAlias = NewType("URL", str)
+Token: TypeAlias = NewType("Token", object)
+
+# Constants
+URL_KEY_INFO: Final[URL] = URL("https://api.guildwars2.com/v2/tokeninfo")
+URL_ACCOUNT_INVENTORY: Final[URL] = URL(
+    "https://api.guildwars2.com/v2/account/inventory"
+)
+URL_BANK_INVENTORY: Final[URL] = URL("https://api.guildwars2.com/v2/account/bank")
+URL_BANK_MATERIALS: Final[URL] = URL("https://api.guildwars2.com/v2/account/materials")
+URL_CHARACTER_LIST: Final[URL] = URL("https://api.guildwars2.com/v2/characters/")
+URL_CHARACTER_INVENTORY_TPL = (
+    "https://api.guildwars2.com/v2/characters/{character}/inventory"
+)
+
+HTTP_OK: Final[int] = 200
 
 
-class APIKey:
-    def __init__(self):
-        """Initialize Key object.
+class KeyPermissions(TypedDict):
+    """
+    Stores which permissions are available on an API key
+    """
 
-        The function automatically tries to load a key from a file.
+    wallet: bool
+    inventories: bool
+    characters: bool
+
+
+class _SlotID(TypedDict):
+    id: str
+
+
+class Slot(_SlotID, TypedDict, total=False):
+    charges: int
+    count: int
+    value: int
+
+
+@define
+class GW2APIError(Exception):
+    """
+    Base class for API related error
+    """
+
+    msg: str
+
+
+@autoformat
+@define
+class APIKeyError(GW2APIError):
+    """Base class for API Key-related errors"""
+
+    key: APIKey
+
+
+@autoformat
+@define
+class InvalidAPIKeyError(APIKeyError):
+    msg: str = "Invalid API Key {key}"
+
+
+@autoformat
+@define
+class KeyPermissionError(APIKeyError):
+    """
+    Insufficient permission on an API key
+    """
+
+    missing_perms: tuple[str, ...]
+    msg: str = "API Key {key} does not have the required permissions {missing_perms}"
+
+
+class AsyncIOThread(threading.Thread):
+    """
+    Thread running an asyncio event loop
+
+    This thread defaults to being a daemon thread. You should use the
+    ``new`` method to create a new instance of this class without manually
+    passing an asyncio loop.
+
+    Parameters:
+        loop: asyncio loop to run in another thread. It must have been created
+            in the main thread with `asyncio.new_event_loop`
+        group: passed to `threading.Thread` constructor
+        name: passed to `threading.Thread` constructor
+        daemon: passed to `threading.Thread` constructor
+    """
+
+    loop: asyncio.AbstractEventLoop
+
+    @classmethod
+    def new(cls: Type[T], **kwargs) -> T:
         """
-        # To avoid copy-pasta : store key location in this variable.
-        self.keyLocation = "Application_data/API_key.txt"
-
-        # Create parent folder if it does not already exist
-        if not os.path.exists("Application_data"):
-            os.makedirs("Application_data")
-
-        # try to load a previously saved key
-        if os.path.isfile(self.keyLocation):
-            with open(self.keyLocation, "r") as f:
-                try:
-                    self.keyValue = f.read()
-                    print("Loaded saved key.")
-                except ValueError as error:
-                    print("Found saved key but is was not valid.")
-                    print(error)
-                    self.__keyValue = ""
-
-        else:
-            print("No saved key found.")
-            self.__keyValue = ""
-
-    @property
-    def keyValue(self):
-        """Model.keyValue getter"""
-        return self.__keyValue
-
-    @keyValue.setter
-    def keyValue(self, new_value):
-        """Model.keyValue setter
-
-        Check that the API key given in argument possesses all the authorizations
-        needed for this tool.
-
-        Authorizations needed :
-            - wallet
-            - inventories
-            - characters (to get list of characters, which allows to fetch single
-              character inventory)
+        Creates a new asyncio event loop and associated AsyncIOThread
         """
-        result = gw2h.GW2_API_handler(
-            "https://api.guildwars2.com/v2/tokeninfo", new_value
+        if threading.current_thread() is not threading.main_thread():
+            raise RuntimeError("Can only start an asyncio thread from the main thread")
+        loop = asyncio.new_event_loop()
+        return cls(loop, **kwargs)
+
+    def __init__(
+        self, loop: asyncio.AbstractEventLoop, *, group=None, name=None, daemon=True
+    ):
+        super().__init__(group=group, name=name, daemon=daemon)
+        self.loop = loop
+
+    def run(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
+
+    @functools.wraps(AbstractEventLoop.call_soon_threadsafe)
+    def call_soon(self, callback, *args):
+        """
+        Schedule a callback to be run by asyncio in this thread
+        """
+        self.loop.call_soon_threadsafe(callback, *args)
+
+
+def get_headers(key: APIKey) -> dict[str, str]:
+    return {"Authorization": f"Bearer {key}"}
+
+
+def get_key_permissions(key: APIKey) -> KeyPermissions | None:
+    response = requests.get(URL_KEY_INFO, headers=get_headers(key))
+    if response.status_code != HTTP_OK:
+        return None
+    else:
+        content = response.json()
+        permissions = content["permissions"]
+        return KeyPermissions(
+            wallet="wallet" in permissions,
+            inventories="inventories" in permissions,
+            characters="characters" in permissions,
         )
 
-        if result == "":
-            raise ValueError(
-                f"Unable to check permissions for key {new_value} ! Check that the key is valid !"
+
+def validate_key(key: APIKey):
+    perms = get_key_permissions(key)
+    if perms is None:
+        raise InvalidAPIKeyError(key=key)
+    if not all(perms.values()):
+        missing_perms = [k for k, v in perms.items() if not v]
+        raise PermissionError(*missing_perms)
+
+
+def is_key_valide(key: APIKey) -> bool:
+    try:
+        validate_key(key)
+        return True
+    except GW2APIError:
+        return False
+
+
+def unwrap_slot(slot: Slot) -> tuple[str, int]:
+    """
+    Unwrap a slots to a tuple, usable to init a dictionary
+
+    If the slot is None, return ("", 0)
+    """
+    if slot:
+        for k in ("charges", "count", "value"):
+            if k in slot:
+                count = slot[k]
+                break
+        else:
+            raise ValueError(f"Invalid slot: {slot}")
+    return slot["id"], count
+
+
+async def call_api(session: aiohttp.ClientSession, url: URL, key: APIKey) -> Any:
+    headers = get_headers(key)
+    async with session.get(url, headers=headers) as response:
+        if response.status == HTTP_OK:
+            return await response.json()
+        else:
+            raise GW2APIError(f"Could not reach {url}: {response}")
+
+
+async def get_account_inventory(
+    session: aiohttp.ClientSession, key: APIKey
+) -> Inventory:
+    slots: list[Slot] = await call_api(session, URL_ACCOUNT_INVENTORY, key)
+    return Inventory(dict(map(unwrap_slot, slots)))
+
+
+async def get_bank_inventory(session: aiohttp.ClientSession, key: APIKey) -> Inventory:
+    slots: list[Slot] = await call_api(session, URL_BANK_INVENTORY, key)
+    return Inventory(dict(map(unwrap_slot, slots)))
+
+
+async def get_bank_materials(session: aiohttp.ClientSession, key: APIKey) -> Inventory:
+    slots: list[Slot] = await call_api(session, URL_BANK_MATERIALS, key)
+    return Inventory(dict(map(unwrap_slot, slots)))
+
+
+async def get_character_inventory(
+    session: aiohttp.ClientSession, key: APIKey, character: str
+) -> Inventory:
+    # TODO: manually encode character name or aiohttp does it ?
+    url = URL(URL_CHARACTER_INVENTORY_TPL.format(character=character))
+    bags: list[list[Slot]] = await call_api(session, url, key)
+    return sum(
+        (Inventory(dict(map(unwrap_slot, bag))) for bag in bags if bag),
+        start=Inventory(),
+    )
+
+
+async def get_characters_inventories(
+    session: aiohttp.ClientSession, key: APIKey
+) -> Inventory:
+    character_names: list[str] = await call_api(session, URL_CHARACTER_LIST, key)
+    inventories = await asyncio.gather(
+        *(
+            get_character_inventory(session, key, character)
+            for character in character_names
+        )
+    )
+    return sum(inventories, start=Inventory())
+
+
+async def get_aggregated_inventory(
+    session: aiohttp.ClientSession, key: APIKey
+) -> Inventory:
+    inventories = await asyncio.gather(
+        *(
+            coro(session, key)
+            for coro in (
+                get_account_inventory,
+                get_bank_inventory,
+                get_bank_materials,
+                get_characters_inventories,
             )
-
-        if "wallet" not in result["permissions"]:
-            print("Missing wallet permission")
-            raise ValueError(f"Key {self.keyValue} is missing wallet permission !")
-        elif "inventories" not in result["permissions"]:
-            print("Missing inventories permission")
-            raise ValueError(f"Key {self.keyValue} is missing inventories permission !")
-        elif "characters" not in result["permissions"]:
-            print("Missing characters permission")
-            raise ValueError(f"Key {self.keyValue} is missing characters permission !")
-
-        print("Valid key with appropriate permissions : OK.")
-        self.__keyValue = new_value
-
-        with open(self.keyLocation, "w") as f:
-            f.write(new_value)
-
-
-class APIHandler:
-    """
-    Class handling the GW2 API
-
-
-    """
-
-    # TODO
+        )
+    )
+    return sum(inventories, start=Inventory())
 
 
 def GW2_API_handler(url, api_key=""):
@@ -117,10 +284,3 @@ def GW2_API_handler(url, api_key=""):
     else:
         return ""
         # raise ValueError(f'Invalid request at {url} for key {api_key} : {response.status_code}')
-
-
-# Main for debug and testing
-if __name__ == "__main__":
-    url = "https://api.guildwars2.com/v2/items/12452"
-    response = GW2_API_handler(url)
-    print(response)
--- gw2_tracker/inventory.py
+++ gw2_tracker/inventory.py
@@ -13,12 +13,13 @@ from __future__ import annotations
 import json
 import threading
 import types
+import typing
 from collections import abc
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import IO, Any, Mapping
 
-from gw2_tracker import gw2_api
+import pendulum
 
 
 @dataclass(frozen=True)
@@ -37,7 +38,7 @@ class Inventory(abc.Mapping[str, int]):
     content: Mapping[str, int] = field(default_factory=dict)
 
     @classmethod
-    def from_file(cls, file: str | Path | IO[bytes | str]) -> Inventory:
+    def from_file(cls, file: str | Path | IO[bytes] | IO[str]) -> Inventory:
         """
         Deserialize an `Inventory` from a file
 
@@ -51,8 +52,10 @@ class Inventory(abc.Mapping[str, int]):
             ValueError: the file is not in the correct format
         """
         if hasattr(file, "read"):
+            file = typing.cast(IO, file)
             content = json.loads(file.read())
         else:
+            file = typing.cast(str | Path, file)
             with Path(file).open("rt", encoding="utf-8") as f:
                 content = json.load(f)
         return cls.from_json(content)
@@ -76,7 +79,7 @@ class Inventory(abc.Mapping[str, int]):
             and all(isinstance(k, str) and isinstance(v, int) for k, v in obj.items())
         ):
             raise ValueError(f"json should be a dict[str, int], got {obj}")
-        return Inventory(obj)
+        return cls(obj)
 
     def __post_init__(self):
         # make content immutable
@@ -118,7 +121,7 @@ class Inventory(abc.Mapping[str, int]):
             return all(v < other for v in self.values())
         elif isinstance(other, Inventory):
             diff = other - self
-            return diff and diff > 0
+            return bool(diff) and (diff > 0)
         return NotImplemented
 
     def __le__(self, other: int | Inventory) -> bool:
@@ -140,7 +143,7 @@ class Inventory(abc.Mapping[str, int]):
             return all(v > other for v in self.values())
         elif isinstance(other, Inventory):
             diff = self - other
-            return diff and diff > 0
+            return bool(diff) and (diff > 0)
 
     def to_json(self) -> dict[str, int]:
         """Serialize the instance to json format"""
@@ -156,281 +159,17 @@ class Inventory(abc.Mapping[str, int]):
         kwargs = kwargs | dict(indent=indent, sort_keys=sort_keys)
         content = self.to_json()
         if hasattr(file, "write"):
+            file = typing.cast(IO[str], file)
             json.dump(content, file, **kwargs)
         else:
+            file = typing.cast(str | Path, file)
             with Path(file).open("wt", encoding="utf-8") as fp:
                 json.dump(content, fp, **kwargs)
 
-    ## Old API - Remove
 
-    @classmethod
-    def load_from_file(cls, source_file, api_key_to_match):
-        """Open source_file and load the inventory stored there into items attributes.
-
-        Arguments :
-            - source_file : location where to find the saved inventory
-            - api_key_to_match : API key the application is currently using.
-            If the saved inventory used a different key, loading is cancelled.
-        """
-        with open(source_file, "r") as f:
-            print("Found saved reference inventory.")
-            fileContent = json.load(f)
-            if api_key_to_match == fileContent["API key"]:
-                # Extract key and save it separately
-                del fileContent["API key"]
-                print("Loaded saved reference inventory.")
-                return cls(items=fileContent)
-            else:
-                print(
-                    "API key from reference inventory does not match current API key. Discarded saved inventory."
-                )
-                raise ValueError(
-                    f'Loading cancelled : API keys do not match. \
-                                  Key from saved inventory is {fileContent["API key"]} \
-                                  while expected key is {api_key_to_match}'
-                )
-
-    def save_to_file(self, target_file, api_key):
-        """Save the inventory to a file.
-
-        Arguments :
-            - filename in which to save the inventory.
-            - key used to get the data
-
-        Note :
-            - if file already exist, it will be overwritten.
-            - The API key will saved as an item amongst the other.
-        """
-        with open(target_file, "w") as outfile:
-            tmp_dict = self.items.copy()
-            tmp_dict["API key"] = api_key
-
-            json.dump(tmp_dict, outfile, indent=3, ensure_ascii=False)
-            print("Saved start inventory into file.")
-
-    def _get_item_data_from_slot(self, inventory_slot):
-        """Internal function. Parse the slot data returned by the API
-
-        Argument : the dict data describing the object contained in a slot
-        Output : a 2 elements tuple (item id, item count)
-        If the item has a 'charge' attribute, this value will replace the 'count'
-        attribute in the output. Items with charges are not stackable anyway.
-
-        /!\ This function assumes the input is not None' i.e slot is non-empty.
-        """
-        item_id = inventory_slot["id"]
-
-        if "charges" in inventory_slot:  # We care about charges for consumable items
-            item_count = inventory_slot["charges"]
-        elif "count" in inventory_slot:
-            item_count = inventory_slot["count"]
-        elif "value" in inventory_slot:
-            item_count = inventory_slot[
-                "value"
-            ]  # Wallet currencies use 'value' instead of 'count' attribute
-        else:
-            raise ValueError("Error while parsing data coming from an inventory slot.")
-
-        return (item_id, item_count)
-
-    def _get_account_generic_data(
-        self, api_url, api_key, ptr_lock, ptr_unaggregatedItemResults
-    ):
-        """Internal function. Get the requested account data via API.
-
-        Arguments :
-            - api_url: valid GW2 API url
-            - api_key : key with appropriate authorization level to access the data (provided by model object)
-            - ptr_unaggregatedItemResults : a variable to fill with the calculation result
-            - ptr_lock : lock object to allow several thread to fill ptr_unaggregatedItemResults
-        Output :
-            list of objects where object contain the following : id, count (or charge when applicable)
-            Output is written directly in parent function variable with a lock constraint
-
-        Typically used to get shared inventory, bank and wallet.
-        """
-
-        api_output = gw2_api.GW2_API_handler(api_url, api_key)
-
-        # print(f'Output of call to {api_url} : {api_output}')
-        """file = "debug/account_generic_data.txt"
-        with open(file, 'a') as f:
-            json.dump({"api_url" : api_url}, f, indent=3, ensure_ascii=False)
-            json.dump(api_output, f, indent=3, ensure_ascii=False)
-        """
-        content = list()
-
-        if api_output != "":
-            for slot in api_output:  # Process each slot in shared inventory
-                if slot != None:
-                    content.append(self._get_item_data_from_slot(slot))
-
-        with ptr_lock:
-            ptr_unaggregatedItemResults += content
-
-    def _get_inventory_of_single_character(
-        self, character_name, api_key, ptr_lock, ptr_unaggregatedItemResults
-    ):
-        """Internal function. Retrieve inventory of a single character of an account via API.
-
-        Arguments :
-            - character name : the character for which the function should retrieve the inventory
-            - api key : key with appropriate authorization level to access the data (provided by model object)
-            - ptr_unaggregatedItemResults : a variable to fill with the calculation result
-            - ptr_lock : lock object to allow several thread to fill ptr_unaggregatedItemResults
-        Output :
-            list of objects where object contain the following : id, count (or charge when applicable)
-            Output is written directly in parent function variable with a lock constraint
-        """
-        # Build API URL
-        character_inventory_url = "https://api.guildwars2.com/v2/characters/"
-        character_inventory_url += character_name.replace(
-            " ", "%20"
-        )  # Percent encoding required by GW2 API
-        character_inventory_url += "/inventory"
-
-        # Get API answer
-        character_inventory = gw2_api.GW2_API_handler(character_inventory_url, api_key)
-
-        # Parse API answer : inventory bag per bag
-        character_items = []
-
-        for bag in character_inventory["bags"]:
-            if bag != None:  # None means bag is empty
-                for slot in bag[
-                    "inventory"
-                ]:  # items are stored in an inventory element
-                    if slot != None:  # empty slots are 'null' string instead of dict
-                        character_items.append(self._get_item_data_from_slot(slot))
-
-        """file = "debug/character_inventories.txt"
-        with open(file, 'a') as f:
-            json.dump(character_inventory, f, indent=3, ensure_ascii=False)
-        """
-
-        # save results
-        with ptr_lock:
-            ptr_unaggregatedItemResults += character_items
-
-    def _get_materials_bank(self, api_key, ptr_lock, ptr_unaggregatedItemResults):
-        """Internal function. Retrieve material inventory of an account via API.
-
-        Argument :
-            - api key : key with appropriate authorization level to access the data (provided by model object)
-            - ptr_unaggregatedItemResults : a variable to fill with the calculation result
-            - ptr_lock : lock object to allow several thread to fill ptr_unaggregatedItemResults
-
-        Output : list of objects where object contain the following : id, count (or charge when applicable)
-
-        ? Maybe this function would be replaced by a call to get_account_generic_data
-        """
-
-        materials = gw2_api.GW2_API_handler(
-            "https://api.guildwars2.com/v2/account/materials", api_key
-        )
-
-        """with open('debug/materials_in_bank.txt', 'w') as f:
-            json.dump(materials, f, indent=3, ensure_ascii=False)
-        """
-
-        parsed_materials = list()
-
-        for item in materials:
-            if (
-                item["count"] > 0
-            ):  # This condition is the only reason this funtion exists separately
-                parsed_materials.append((item["id"], item["count"]))
-
-        with ptr_lock:
-            ptr_unaggregatedItemResults += parsed_materials
-
-    def get_full_inventory(self, api_key):
-        """Get list of player owned items from various sources :
-        - banks (items and crafting materials)
-        - inventories (shared and characters specific)
-        Input : a valid api key for player account (provided by model object)
-        Output : dictionnary (key = item id, value = item count/charges)
-
-        Note : wallet is handled separately. Not in this function.
-
-        Note 2 : all API calls are made simultaneously.
-        """
-
-        # A raw concatenation/merge of items lists so one item can have several entries.
-        unaggregatedItemResults = []
-
-        lock = (
-            threading.Lock()
-        )  # to make sure that no thread collides when saving results.
-
-        threads = []
-
-        self.items = dict()  # reset inventory data
-
-        print("Get full inventory")
-
-        # Get shared inventory
-        print("  Get shared inventory...")
-        threads.append(
-            threading.Thread(
-                target=self._get_account_generic_data,
-                args=(
-                    "https://api.guildwars2.com/v2/account/inventory",
-                    api_key,
-                    lock,
-                    unaggregatedItemResults,
-                ),
-            )
-        )
-
-        # Get materials bank content
-        print("  Get materials in bank...")
-        threads.append(
-            threading.Thread(
-                target=self._get_materials_bank,
-                args=(api_key, lock, unaggregatedItemResults),
-            )
-        )
-
-        # Get characters inventories
-        print("  Get characters inventories...")
-        characters_list = gw2_api.GW2_API_handler(
-            "https://api.guildwars2.com/v2/characters/", api_key
-        )
-        for character in characters_list:
-            threads.append(
-                threading.Thread(
-                    target=self._get_inventory_of_single_character,
-                    args=(character, api_key, lock, unaggregatedItemResults),
-                )
-            )
-
-        # Get bank content
-        print("  Get bank vault content...")
-        threads.append(
-            threading.Thread(
-                target=self._get_account_generic_data,
-                args=(
-                    "https://api.guildwars2.com/v2/account/bank",
-                    api_key,
-                    lock,
-                    unaggregatedItemResults,
-                ),
-            )
-        )
-
-        for thread in threads:
-            thread.start()
-
-        for thread in threads:
-            thread.join()
-
-        # Aggregate data into self.referenceInventory
-        print("Full inventory retrieved. Aggregating raw data...")
-        for item in unaggregatedItemResults:
-            if item[0] in self.items:
-                self.items[item[0]] += item[1]
-            else:
-                self.items[item[0]] = item[1]
-
-        print("Data aggregated. Inventory fetch complete.")
+@dataclass
+class Snapshot:
+    api_key: str
+    inventory: Inventory
+    timestamp = field(default_factory=pendulum.now)
+    name: None | str = None
