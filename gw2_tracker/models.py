024521b088cceb37c4e06904d4dc71066ff4fb46
Quentin Soubeyran <45202794+QuentinSoubeyran@users.noreply.github.com>
2021-11-21 16:43:28 +0100

    moved all model to models.py; started new model

---
+++ gw2_tracker/models.py
@@ -0,0 +1,394 @@
+# -*- coding: utf-8 -*-
+"""
+Model layer of GW2 tool to evaluate gold earnings.
+
+Main features :
+    - set API key (Model.apiKey)
+    - get inventory and define it as reference
+    - get inventory and compare it to the reference
+    - generates full report for user
+
+@author: Krashnark
+"""
+
+from __future__ import annotations
+
+import json
+import os.path
+import types
+import typing
+from collections import abc
+from pathlib import Path
+from typing import IO, Any, Mapping, NewType, TypeAlias
+
+import attr
+import pendulum
+from attr import field, frozen, mutable
+from pendulum.datetime import DateTime
+
+from gw2_tracker import utils
+
+APIKey: TypeAlias = NewType("APIKey", str)
+
+
+@frozen
+class Inventory(abc.Mapping[str, int]):
+    """
+    Immutable mapping from string ID to non-zero amounts
+
+    Includes utility methods such as comparisons, serialization
+    and addition/substraction. Inventory object can be compared to int
+    and to themselves. Comparison to int is equivalent to comparing
+    all values to the int. Comparison to another inventory compares the keys
+    as sets and the values one-by-one. This is performed by checking the
+    difference of the compared set to 0.
+    """
+
+    content: Mapping[str, int] = field(factory=dict)
+
+    @classmethod
+    def from_file(cls, file: str | Path | IO[bytes] | IO[str]) -> Inventory:
+        """
+        Deserialize an `Inventory` from a file
+
+        Arguments:
+            file: file name, `Path` or opened file to read from
+
+        Returns:
+            deserialized `Inventory` object
+
+        Raises:
+            ValueError: the file is not in the correct format
+        """
+        if hasattr(file, "read"):
+            file = typing.cast(IO, file)
+            content = json.loads(file.read())
+        else:
+            file = typing.cast(str | Path, file)
+            with Path(file).open("rt", encoding="utf-8") as f:
+                content = json.load(f)
+        return cls.from_json(content)
+
+    @classmethod
+    def from_json(cls, obj: Any) -> Inventory:
+        """
+        Deserialize an `Inventory` from an already loaded JSON
+
+        Argument:
+            obj: json object to deserialize
+
+        Returns:
+            deserialized `Inventory` object
+
+        Raises:
+            ValueError: the json object is not in the correct format
+        """
+        if not (
+            isinstance(obj, dict)
+            and all(isinstance(k, str) and isinstance(v, int) for k, v in obj.items())
+        ):
+            raise ValueError(f"json should be a dict[str, int], got {obj}")
+        return cls(obj)
+
+    def __attrs_post_init__(self):
+        # make content immutable
+        object.__setattr__(
+            self,
+            "content",
+            types.MappingProxyType({k: v for k, v in self.content.items() if v}),
+        )
+
+    def to_json(self) -> dict[str, int]:
+        """Serialize the instance to json format"""
+        return dict(self.content)
+
+    def to_file(self, file: str | Path | IO[str], indent=4, sort_keys=True, **kwargs):
+        """Serialize the instance to json format and write to the file
+
+        Arguments:
+            file: file name, `Path` or `IO` object to write to
+            kwargs: additional keywords argument to pass to `json.dump`
+        """
+        kwargs = kwargs | dict(indent=indent, sort_keys=sort_keys)
+        content = self.to_json()
+        if hasattr(file, "write"):
+            file = typing.cast(IO[str], file)
+            json.dump(content, file, **kwargs)
+        else:
+            file = typing.cast(str | Path, file)
+            with Path(file).open("wt", encoding="utf-8") as fp:
+                json.dump(content, fp, **kwargs)
+
+    def __getitem__(self, key: str) -> int:
+        return self.content[key]
+
+    def __len__(self) -> int:
+        return len(self.content)
+
+    def __iter__(self) -> abc.Iterator[str]:
+        return iter(self.content)
+
+    def __add__(self, other: Inventory) -> Inventory:
+        if isinstance(other, Inventory):
+            return Inventory(
+                {
+                    k: self.get(k, 0) + other.get(k, 0)
+                    for k in self.keys() | other.keys()
+                }
+            )
+        return NotImplemented
+
+    def __sub__(self, other: Inventory) -> Inventory:
+        if isinstance(other, Inventory):
+            return Inventory(
+                {
+                    k: self.get(k, 0) - other.get(k, 0)
+                    for k in self.keys() | other.keys()
+                }
+            )
+        return NotImplemented
+
+    def __lt__(self, other: int | Inventory) -> bool:
+        if isinstance(other, int):
+            return all(v < other for v in self.values())
+        elif isinstance(other, Inventory):
+            diff = other - self
+            return bool(diff) and (diff > 0)
+        return NotImplemented
+
+    def __le__(self, other: int | Inventory) -> bool:
+        if isinstance(other, int):
+            return all(v <= other for v in self.values())
+        elif isinstance(other, Inventory):
+            return (other - self) >= 0
+        return NotImplemented
+
+    def __ge__(self, other: int | Inventory) -> bool:
+        if isinstance(other, int):
+            return all(v >= other for v in self.values())
+        elif isinstance(other, Inventory):
+            return (self - other) >= 0
+        return NotImplemented
+
+    def __gt__(self, other: int | Inventory) -> bool:
+        if isinstance(other, int):
+            return all(v > other for v in self.values())
+        elif isinstance(other, Inventory):
+            diff = self - other
+            return bool(diff) and (diff > 0)
+
+
+@frozen
+class Snapshot:
+    key: APIKey
+    inventory: Inventory
+    datetime: DateTime = field(factory=pendulum.now)
+    name: None | str = None
+
+    @classmethod
+    def from_file(cls, file: str | Path | IO[bytes] | IO[str]) -> Snapshot:
+        """
+        Deserialize a `Snapshot` from a file
+
+        Arguments:
+            file: file name, `Path` or opened file to read from
+
+        Returns:
+            deserialized object
+
+        Raises:
+            ValueError: the file is not in the correct format
+        """
+        if hasattr(file, "read"):
+            file = typing.cast(IO, file)
+            content = json.loads(file.read())
+        else:
+            file = typing.cast(str | Path, file)
+            with Path(file).open("rt", encoding="utf-8") as f:
+                content = json.load(f)
+        return cls.from_json(content)
+
+    @classmethod
+    def from_json(cls, obj: Any) -> Snapshot:
+        """
+        Deserialize an `Inventory` from an already loaded JSON
+
+        Argument:
+            obj: json object to deserialize
+
+        Returns:
+            deserialized object
+
+        Raises:
+            ValueError: the json object is not in the correct format
+        """
+        if not isinstance(obj, dict):
+            raise ValueError(f"expected a JSON object, got {obj}")
+
+        fields = utils.unjsonize(cls, obj)
+        return Snapshot(
+            key=APIKey(fields["key"]),
+            inventory=fields["inventory"],
+            datetime=typing.cast(DateTime, pendulum.parser.parse(fields["datetime"])),
+            name=fields.get("name"),
+        )
+
+    def to_json(self) -> utils.JsonObject:
+        """Serialize the instance to JSON"""
+        fields = utils.jsonize(self)
+        fields["datetime"] = fields["datetime"].isoformat()
+        return fields
+
+    def to_file(self, file: str | Path | IO[str], indent=4, sort_keys=True, **kwargs):
+        """Serialize the instance to json format and write to the file
+
+        Arguments:
+            file: file name, `Path` or `IO` object to write to
+            kwargs: additional keywords argument to pass to `json.dump`
+        """
+        kwargs = kwargs | dict(indent=indent, sort_keys=sort_keys)
+        content = self.to_json()
+        if hasattr(file, "write"):
+            file = typing.cast(IO[str], file)
+            json.dump(content, file, **kwargs)
+        else:
+            file = typing.cast(str | Path, file)
+            with Path(file).open("wt", encoding="utf-8") as fp:
+                json.dump(content, fp, **kwargs)
+
+
+@mutable
+class Model:
+    """
+    Save the application state
+    """
+
+    filepath: Path = field(converter=Path)
+    current_key: None | APIKey = None
+    data: dict[APIKey, list[Snapshot]] = field(factory=dict)
+
+    @classmethod
+    def from_file(cls, filepath: str | Path) -> Model:
+        """
+        Deserialize a model from a file and make the model save itself
+        to this file
+        """
+        filepath = Path(filepath)
+
+        with filepath.open("rt", encoding="utf-8") as file:
+            obj = json.load(file)
+
+        if not isinstance(obj, dict):
+            raise ValueError(f"{filepath} does not contain a json object")
+
+        fields = utils.unjsonize(cls, obj, ignore=["filepath"])
+        return Model(filepath=filepath, **fields)
+
+    def save(self):
+        """Save the model to the given file"""
+        obj = utils.jsonize(self, ignore=["filepath"])
+        with self.filepath.open("wt", encoding="utf-8") as file:
+            json.dump(obj, file)
+
+
+# class Model:
+#     def __init__(self, api_key=""):
+#         """
+#         Model initialization.
+
+#         self.applicationState possible values :
+#             - "0 - started"
+#             - "1 - got api key"
+#             - "2 - got start inventory"
+#             - "3 - got end inventory"
+#             - "4 - got full report"
+#         """
+#         self.applicationState = "0 - started"
+#         self.apiKey = (
+#             gw2_api.APIKey()
+#         )  # Declare API key, try to load a previously saved API key
+#         self.referenceInventory = Inventory()
+#         self.newInventory = Inventory()
+#         self.report = report.Report()
+
+#         if self.apiKey.keyValue != "":
+#             self.applicationState = "1 - got api key"
+
+#         if self.applicationState == "1 - got api key":
+#             # Try to load saved reference inventory
+#             if os.path.isfile("Application_data/start_inventory.txt"):
+#                 self.referenceInventory.load_from_file(
+#                     "Application_data/start_inventory.txt", self.apiKey.keyValue
+#                 )
+#                 self.applicationState = "2 - got start inventory"
+#             else:
+#                 print("Could not find saved and valid reference inventory.")
+
+#         # This loading do not make sense without loading the full report
+#         # if self.applicationState == "2 - got start inventory" :
+#         #     # try to load target inventory
+#         #     try:
+#         #         self.referenceInventory.load_from_file("Application_data/end_inventory.txt", self.apiKey)
+#         #         self.applicationState = "3 - got end inventory"
+#         #     except ValueError as error :
+#         #         print ("Could not find saved and valid reference inventory.")
+#         #         print (error)
+
+#         # Maybe will implement load function for report.
+#         # Maybe...
+
+#     def set_new_key(self, new_key):
+#         """User set a new key. Let's validate it and save it."""
+#         try:
+#             self.apiKey.keyValue = new_key
+#             self.applicationState = "1 - got api key"
+#         except:
+#             pass
+
+#     def set_reference_inventory(self):
+#         """Get full inventory of an account and put it in reference inventory"""
+#         if self.applicationState == "0 - started":
+#             raise ValueError("Key was not yet defined !")
+
+#         self.referenceInventory.get_full_inventory(self.apiKey.keyValue)
+
+#         self.referenceInventory.save_to_file(
+#             "Application_data/start_inventory.txt", self.apiKey.keyValue
+#         )
+
+#         self.applicationState = "2 - got start inventory"
+
+#         # with open("debug/reference_inventory.txt", 'w') as f:
+#         #     json.dump(self.referenceInventory.items, f, indent=3, ensure_ascii=False)
+
+#     def get_inventory_and_compare_it(self):
+#         """
+#         Get full inventory of an account and put it in new/updated inventory.
+#         Then compare reference inventory with new inventory and build the report.
+#         """
+#         if self.applicationState in ("0 - started", "1 - got api key"):
+#             raise ValueError("Missing key or reference inventory !")
+#         self.newInventory.get_full_inventory(self.apiKey.keyValue)
+
+#         self.referenceInventory.save_to_file(
+#             "Application_data/end_inventory.txt", self.apiKey.keyValue
+#         )
+#         self.applicationState = "3 - got end inventory"
+#         self.report.compare_inventories(self.referenceInventory, self.newInventory)
+
+#         print(
+#             f"function get inventory & compare it : report content after comparison :"
+#         )
+#         print(f"   self.report.itemsDetail : {self.report.itemsDetail}")
+
+#         self.report.get_item_details()
+
+#         print(
+#             f"function get inventory & compare it : report content after getting details :"
+#         )
+#         print(f"   self.report.itemsDetail : {self.report.itemsDetail}")
+
+#         self.applicationState = "4 - got full report"
+
+#         # with open("debug/new_inventory.txt", 'w') as f:
+#         #     json.dump(self.newInventory.items, f, indent=3, ensure_ascii=False)
--- gw2_tracker/utils.py
+++ gw2_tracker/utils.py
@@ -2,9 +2,20 @@ from __future__ import annotations
 
 import functools
 import inspect
-from typing import Callable, Iterable, ParamSpec, Type, TypeVar, overload
+from typing import Any, Callable, Iterable, TypeAlias, TypeVar, overload
 
 Cls = TypeVar("Cls", bound=type)
+
+JsonValue: TypeAlias = None | bool | int | float | str
+JsonArray: TypeAlias = list["AnyJson"]
+JsonObject: TypeAlias = dict[str, "AnyJson"]
+AnyJson: TypeAlias = JsonValue | JsonArray | JsonObject
+
+AttrClass: TypeAlias = type
+
+from collections import abc
+
+import attr
 
 
 def required_attrs(*attributes: str):
@@ -99,3 +110,116 @@ def autoformat(cls, /, params=("message", "msg")):
         return decorator(cls)
     else:
         raise TypeError(f"@autoformat is a class decorator, cannot decorate {cls}")
+
+
+def jsonize(inst, *, ignore: abc.Container[str] = ()) -> dict[str, Any]:
+    """
+    Recursively serialize an attr instance to JSON.
+
+    Serialize an attr instance to JSON by returning a dictionary of fields
+    values. If a field value has the ``to_json`` method, it is called to get
+    its json serialization, otherwise the value is left as-is.
+
+    This is intended as a helper to implement a ``to_json`` method on attr
+    classes, which may defer to this function.
+
+    Arguments:
+        instance: attr class instance to serialize to json
+        ignore: field name to ignore and leave out of the serialization
+
+    Returns:
+        The json serialization of instance. All values may not be valid
+        json values, depending on the fields type.
+
+    Raises:
+        attr.NotAnAttrsClassError: instance is not an instance of an attr class
+        AttributeError: if a field is not found on instance
+
+    See also:
+        unjsonize
+    """
+    cls = type(inst)
+    cls_fields = attr.fields(cls)
+
+    def filter(field: attr.Attribute, value: Any):
+        return not (field in cls_fields and field.name in ignore)
+
+    def serializer(instance, field: attr.Attribute, value):
+        if hasattr(value, "to_json"):
+            return value.to_json()
+        return value
+
+    return attr.asdict(inst, recurse=True, filter=filter, value_serializer=serializer)
+    return {
+        f.name: value.to_json()
+        if hasattr((value := getattr(instance, f.name)), "to_json")
+        else value
+        for f in attr.fields(type(instance))
+        if f.name not in ignore
+    }
+
+
+def unjsonize(
+    cls: AttrClass, obj: JsonObject, *, ignore: abc.Container[str] = ()
+) -> dict[str, Any]:
+    """
+    Deserialize an attr class from json format.
+
+    Deserialize an attr class from the json format. This is the inverse
+    operation from `jsonize`, with the some caveats.
+
+    First, only fields whose type is exactly a class will be checked for a
+    ``from_json`` classmethod. Union of any kind of complex typing will not
+    work.
+
+    Second, due to the first caveat, this function will not return an instance
+    of `cls`, but a dict mapping field name from field values. Further
+    deserialization may be performed before constructing an instance with
+    ``cls.__init__(**fields)``, where fields is the return value of this function.
+
+    This is intended as a helper function to implement a ``from_json``
+    classmethod on attr-decorated classes.
+
+    Arguments:
+        cls: class to deserialize against. Must be an att-decorated class
+        obj: json object to deserialize
+        ignore: fields of ``cls`` to ignore. The function behaves exactly as if
+            ``cls`` did not have those fields. In particular, `unjsonize` will
+            raise an error if the fields are found in ``obj``.
+
+    Returns:
+        a dict mapping fields name from deserialized field values.
+
+    Raises:
+
+    """
+    attr.resolve_types(cls)
+    expected_fields = {
+        name: field
+        for name, field in attr.fields_dict(cls).items()
+        if name not in ignore
+    }
+    required_fields = {
+        n: f for n, f in expected_fields.items() if f.default is not attr.NOTHING
+    }
+
+    if not obj.keys() <= expected_fields.keys():
+        raise ValueError(
+            f"json object has invalid fields not found in {cls.__name__}: "
+            + ", ".join(obj.keys() - expected_fields.keys())
+        )
+    if not required_fields.keys() <= obj.keys():
+        raise ValueError(
+            f"json object is missing required fields from {cls.__name__}: "
+            + ", ".join(required_fields.keys() - obj.keys())
+        )
+
+    fields = {}
+    for name, field in expected_fields.items():
+        if name in obj:
+            if isinstance(field.type, type) and hasattr(field.type, "from_json"):
+                fields[name] = field.type.from_json(obj[name])  # type: ignore
+            else:
+                fields[name] = obj[name]
+
+    return fields
