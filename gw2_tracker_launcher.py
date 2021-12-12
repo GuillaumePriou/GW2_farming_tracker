0a352ca5c00676fcc4f3d76596deee3121a2de3a
Quentin Soubeyran <45202794+QuentinSoubeyran@users.noreply.github.com>
2021-12-12 23:21:45 +0100

    switched build to taskipy + python for corss-plateform builds

--- gw2_tracker_launcher.py
+++ gw2_tracker_launcher.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+"""
+Laucher script for the GW2 tracker app.
+
+This script makes it easier to have all pyinstaller-related files at the
+top-level of the project
+"""
 import sys
 if sys.version_info < (3, 10):
     raise RuntimeError("GW2 tracker requires python 3.10+")
